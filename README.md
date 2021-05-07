041da456382f3417229a753ef88dd0b2e3a574c9
shen-ui <peterhuiwang1995@gmail.com>
2021-05-07 17:59:20 -0400

    back end code is working and running. Follow instructions on md to establish a local db.

---
+++ README.md
@@ -0,0 +1,31 @@
+# Updated-ToDoList
+
+An updated ToDo List application built off react. Contains persistent data via a text.json and saved on backend with SQL.
+
+Decided to start small and develope with MySQL database. Database itself is currently being locally hosted on port 3001. Will eventually be migrated once the model has been tested. Code will update accordingly as well as the client side code to push data into the database.
+
+## Steps to run database
+
+1. Download MySQL Workbench 8.0 CE. Make sure you are using legacy password mode during MySQL Server Configurator.
+2. Connect to the `SQL server with MySQL Connections` in the UI.
+    If you used port `3306` for the configurator, connect to the MySQL server via that port. You do not need to connect to port 3333, thats only for the front end to listen to. (I may have wasted a lot of time figuring this out..)
+
+edit the code in index.js here to connect node.js to the actual database.
+
+``
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306, // edit the port here
+    database: 'database_name',
+    user: 'name',
+    password: 'password'
+});
+``
+
+3. Create the table. I have included a example json file for you to create the table. Once you have connected to the SQL server, on the left side of the `Navigator` tab, click, on Schemas at the bottom of the box.
+4. Click on the drop down menu for the database you made and right click on `Tables`
+5. Click on `Table Data Import Wizard` and go to the directory of the `data.json`
+6. Finish the setup and right click on Tables and `refresh` the menu and you should see the table with the name you declared it as.
+7. Left click on the table itself and select `Select Rows - Limit 1000` and you should have a view of the table.
+
+Your database is now established and should be ready for the front end to use!
--- index.js
+++ index.js
@@ -3,23 +3,62 @@ const app = express()
 const mysql = require("mysql");
 
 //login cridentials
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'password',
+    port: 3306,
     database: 'todo_list',
+    user: 'DADDYMUSK',
+    password: '2020TSupra!'
 });
 
 // res: response on the front end
 // req: used to get information from the front end
-app.get('/', (req, res) =>{
-    const sqlInsert = "INSERT INTO todo_list (text, date, complete) VALUES ? (Go to the gym, 4/2/2021 @4:30:32, TRUE)"
-    db.query(sqlInsert, (err, result) => {
-        res.send('hello world');
+app.post("/api/insert", (req, res) => {
+    const sqlInsert = "INSERT INTO todo_list.data (text, date, complete) VALUES ('finish this off', 1995-10-30 14:32:12, false);"
+    //const sqlInsert = "INSERT INTO todo_list (text, date, complete) VALUES (?,?,?)"
+
+    db.query(sqlInsert, [text, date, complete], (err, res) => {  
     })
 });
 
-app.listen(3001, () => {
-    console.log('running on port 3001');
+app.listen(3333, () => {
+    console.log('running on port 3306');
 });
 
+db.connect(function (err) {
+    if(err){
+        console.log("error occured while connecting");
+    }
+    else{
+        console.log("connection created with Mysql successfully");
+    }
+ });
+
+
+
+//--------------------TROUBLESHOOTING FUNCTIONS--------------------------------------
+// Sends data to the front end of localhost
+/*
+app.get("/", (req, res) => {
+    const sqlInsert = "INSERT INTO todo_list (text, date, complete) VALUES ('finish this off', 1995-10-30 14:32:12, false);"
+    res.send(sqlInsert)
+})
+
+db.end((err) => {
+    // The connection is terminated gracefully
+    // Ensures all remaining queries are executed
+    // Then sends a quit packet to the MySQL server.
+ });
+
+ app.get("/", (req, res) => {
+    const sqlInsert = "INSERT INTO todo_list.data (text, date, complete) VALUES ('finish this off', '1995-10-30 14:32:12', 'false');"
+    db.query(sqlInsert, (err, result) => {
+        if(err){
+            console.log("Problem sending data:" + err);
+        }
+        else{
+            res.send("Hello World");
+        }
+    })
+});
+*/
