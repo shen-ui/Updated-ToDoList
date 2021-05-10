89a1612b6d760f043940ca688418f0a859ba742d
shen-ui <peterhuiwang1995@gmail.com>
2021-05-10 17:58:21 -0400

    both post and get functions operational.

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+/**
+ * backend code for the todo list.
+ * 
+ * Really one of the first few times I've used express with node 
+ *                          so I will be commenting what I learn.
+ * 
+ * Ommitting sql comments because sql isn't particularly tedious.
+ * 
+ * app.get() => runs code on going into the given directory
+ * app.post() => runs code upon a api post requested on the front end with a given directory
+ * app.listen() => backend is accessable from the front end with a local host port.
+ * 
+ */
+
 const express = require('express');
 const app = express()
-const mysql = require("mysql");
+const mysql = require('mysql');
+const cors = require('cors');
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
 //login cridentials
 const db = mysql.createConnection({
@@ -10,19 +31,47 @@ const db = mysql.createConnection({
     user: 'DADDYMUSK',
     password: '2020TSupra!'
 });
-
-// res: response on the front end
-// req: used to get information from the front end
+app.use(cors(corsOptions));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.get('/api/get', (req, res) =>{
+    const sqlSelect = "SELECT * FROM data"
+    db.query(sqlSelect, (err, result) => {
+        console.log(result);
+    })
+})
 app.post("/api/insert", (req, res) => {
-    const sqlInsert = "INSERT INTO todo_list.data (text, date, complete) VALUES ('finish this off', 1995-10-30 14:32:12, false);"
-    //const sqlInsert = "INSERT INTO todo_list (text, date, complete) VALUES (?,?,?)"
-
-    db.query(sqlInsert, [text, date, complete], (err, res) => {  
+    const id = req.body.id;
+    const text = req.body.text;
+    const date = req.body.date;
+    const complete = req.body.complete;
+    const sqlInsert = "INSERT INTO data (id, text, date, complete) VALUES (?,?,?,?);"
+    
+    db.query(sqlInsert, [id, text, date, complete], (err, res) => {  
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(res);
+        }
     })
 });
 
+app.post("/api/get", (req, res) => {
+    const sqlSelect =
+    "SELECT * FROM todolist.data";
+    db.query(sqlInsert, [text, date, complete], (err,result) => {
+        if (err){
+            console.log(err)
+        }
+        else{
+        console.log(result);
+        }
+    });
+});
+
 app.listen(3333, () => {
-    console.log('running on port 3306');
+    console.log('running on port 3333');
 });
 
 db.connect(function (err) {
@@ -33,8 +82,6 @@ db.connect(function (err) {
         console.log("connection created with Mysql successfully");
     }
  });
-
-
 
 //--------------------TROUBLESHOOTING FUNCTIONS--------------------------------------
 // Sends data to the front end of localhost
