f6cdccd30ec5232ad2b8018f7aa8f76a276ad6fe
shen-ui <peterhuiwang1995@gmail.com>
2021-05-11 14:49:40 -0400

    delete now working. NOTE: YOU MUST PASS VARIABLES AS PARAMS. Body-parser doesn't work with delete function for node.

--- index.js
+++ index.js
@@ -31,70 +31,61 @@ const db = mysql.createConnection({
     user: 'DADDYMUSK',
     password: '2020TSupra!'
 });
+
 app.use(cors(corsOptions));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get('/api/get', (req, res) =>{
-    const sqlSelect = "SELECT * FROM data"
+app.get('/api/get', (req, res) => {
+    const sqlSelect = 
+        "SELECT * FROM data ORDER BY date DESC";
     db.query(sqlSelect, (err, result) => {
-        res.send(result);
+        res.send(result)
     })
-})
+});
+
+app.delete('/api/delete/:id', (req, res) => {
+    const id = req.params.id;
+    //console.log(id);
+    const sqlDelete = 
+        "DELETE FROM data WHERE id = ?";
+    db.query(sqlDelete, id, (err, result) => {
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log("row deleted: " + id)
+        }
+    })
+});
 
 app.post("/api/insert", (req, res) => {
-    const id = req.body.id;
-    const text = req.body.text;
-    const date = req.body.date;
-    const complete = req.body.complete;
+    console.log(req.body);
+    const id = req.body.id
+    const text = req.body.text
+    const date = req.body.date
+    const complete = req.body.complete
     const sqlInsert = "INSERT INTO data (id, text, date, complete) VALUES (?,?,?,?);"
     
     db.query(sqlInsert, [id, text, date, complete], (err, res) => {  
         if(err){
-            console.log(err);
+            console.log(err)
         }
         else{
-            console.log(res);
+            console.log(res)
         }
     })
 });
 
 app.listen(3333, () => {
-    console.log('running on port 3333');
+    console.log('running on port 3333')
 });
 
 db.connect(function (err) {
     if(err){
-        console.log("error occured while connecting");
+        console.log("error occured while connecting")
     }
     else{
-        console.log("connection created with Mysql successfully");
+        console.log("connection created with Mysql successfully")
     }
  });
-
-//--------------------TROUBLESHOOTING FUNCTIONS--------------------------------------
-// Sends data to the front end of localhost
-/*
-app.get("/", (req, res) => {
-    const sqlInsert = "INSERT INTO todo_list (text, date, complete) VALUES ('finish this off', 1995-10-30 14:32:12, false);"
-    res.send(sqlInsert)
-})
-
-db.end((err) => {
-    // The connection is terminated gracefully
-    // Ensures all remaining queries are executed
-    // Then sends a quit packet to the MySQL server.
- });
-
- app.get("/", (req, res) => {
-    const sqlInsert = "INSERT INTO todo_list.data (text, date, complete) VALUES ('finish this off', '1995-10-30 14:32:12', 'false');"
-    db.query(sqlInsert, (err, result) => {
-        if(err){
-            console.log("Problem sending data:" + err);
-        }
-        else{
-            res.send("Hello World");
-        }
-    })
-});
-*/
